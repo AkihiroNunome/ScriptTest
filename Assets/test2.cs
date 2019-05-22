1f128872f1f4c53f20c9966ec6fe8d029146e450
AkihiroNunome <aki.nunome@gmail.com>
2019-05-22 21:58:24 +0900

    再提出：エラーの修正
    indexの最大値にarray.Lengthを使用していたため、読み込みの際に配列の要素以上の要素を読もうとしていたエラーを修正しました。

--- Assets/test2.cs
+++ Assets/test2.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class test2 : MonoBehaviour {
     
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         int[] array = { 1, 2, 3, 4, 5 };
-        for (int i = 0; i <= array.Length; i++)
+        for (int i = 0; i <= array.Length-1; i++)
         {
             Debug.Log(array[i]);
         }
-        for (int j = array.Length; j >= 0; j -= 1)
+
+        for (int j = array.Length-1; j >= 0; j--)
         {
             Debug.Log(array[j]);
         }
